adea7828422aa461f2bef7a47491529ef94e1066
Ian Harrigan <ianharrigan@hotmail.com>
2017-10-18 13:36:54 +0200

    show one line before top when rendering (looks nicer)

--- textbox/TextBox.hx
+++ textbox/TextBox.hx
@@ -273,6 +273,9 @@ class TextBox
 		{
 			var maxOfLines = Math.ceil(size.y / font.height(fontSize));
 			var topLine = Std.int((scrollOffset.y / font.height(fontSize)));
+            if (topLine != 0) {
+               topLine--; 
+            }
 			var bottomLine = topLine + maxOfLines;
 			var line = topLine;
 			var lastBreak = line > 0 ? breaks[line - 1] : 0;
